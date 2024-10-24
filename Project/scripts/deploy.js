cf2e6ec22f02d9caeea9219c3ba7249f8e5cefa6
krupalll <137489828+krupalll@users.noreply.github.com>
2024-10-24 15:07:15 +0530

    Add files via upload

---
+++ Project/scripts/deploy.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+async function main() {
+  const Voting = await ethers.getContractFactory("Voting");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const Voting_ = await Voting.deploy(
+    ["Harshil", "Anshu", "Purva", "Pujan", "Vedant", "Krupal"],
+    15
+  );
+
+  console.log("Contract address:", Voting_.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
